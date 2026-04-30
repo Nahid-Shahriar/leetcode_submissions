333a7c51c1f6874ca79fa013d94d472f64ca0761
Nahid Shahriar Rafi <nahidrafi99@gmail.com>
2026-04-30 18:04:20 +0600

    Create 1071. Greatest common Divisor of Strings.py

---
+++ 1071. Greatest common Divisor of Strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        a = len(str1)
+        b = len(str2)
+        
+        while b:
+            a, b = b, a % b
+        
+        
+        return str1[:a]
+
+
+mysol = Solution()
+print(mysol.gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"))
